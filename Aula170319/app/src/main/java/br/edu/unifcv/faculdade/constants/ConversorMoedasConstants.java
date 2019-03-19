bdf0a7b58e9229c00f55e9551f148d44050fd74a
Caio Cesar Dantas <dantascaiocesar@gmail.com>
2019-03-18 21:12:52 -0300

    Eita

--- Aula170319/app/src/main/java/br/edu/unifcv/faculdade/constants/ConversorMoedasConstants.java
+++
@@ -1,8 +0,0 @@
-package br.edu.unifcv.faculdade.constants;
-
-public class ConversorMoedasConstants {
-
-    public static final String COTACAO_DOLAR = "cotacao_dollar";
-    public static final String COTACAO_EURO = "cotacao_euro";
-    public static final String VALOR_REAL = "valor_real";
-}
